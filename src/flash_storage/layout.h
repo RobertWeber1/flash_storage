9e3f4ae5efbdf0add8640dff1386b81019058274
Robert Weber <robert.weber@fabmatics.com>
2020-05-15 23:28:51 +0200

    Improve Layout-Struct interface, remove c++17 fold expression

--- src/flash_storage/layout.h
+++ src/flash_storage/layout.h
@@ -4,8 +4,37 @@
 namespace flash_storage
 {
 
-template<uint32_t BaseAddress, size_t ... Sectors>
-struct Layout
+template<class T>
+struct greater
+{
+	constexpr bool operator()(const T &lhs, const T &rhs) const
+	{
+	    return lhs > rhs;
+	}
+};
+
+template<class T>
+struct equal_to
+{
+	constexpr bool operator()(const T &lhs, const T &rhs) const
+	{
+	    return lhs == rhs;
+	}
+};
+
+template<uint32_t Value>
+struct BaseAddress
+{};
+
+template<uint32_t ... Values>
+struct Sizes
+{};
+
+template<class, class>
+struct Layout;
+
+template<uint32_t Address, uint32_t ... Sectors>
+struct Layout<BaseAddress<Address>, Sizes<Sectors...>>
 {
 	static constexpr size_t sector_count()
 	{
@@ -16,27 +45,28 @@ struct Layout
 	static constexpr uint32_t sector_offset()
 	{
 		static_assert(I < sector_count(), "");
-		return offset_<I>(std::make_index_sequence<sector_count()>{});
+		return
+			Address +
+			for_each_<greater<size_t>, I>(
+				std::make_index_sequence<sector_count()>{});
 	}
 
 	template<size_t I>
-	static constexpr size_t sector_size()
+	static constexpr uint32_t sector_size()
 	{
 		static_assert(I < sector_count(), "");
-		return size_<I>(std::make_index_sequence<sector_count()>{});
+		return for_each_<equal_to<size_t>,I>(
+			std::make_index_sequence<sector_count()>{});
 	}
 
 private:
-	template<size_t I, size_t ... Is>
-	static constexpr uint32_t offset_(std::index_sequence<Is...> const&)
+	template<class Func, size_t I, size_t ... Is>
+	static constexpr uint32_t for_each_(
+		std::index_sequence<Is...> const&,
+		uint32_t accu = 0)
 	{
-		return BaseAddress + ( (Sectors*((I>Is)?1:0)) + ... );
-	}
-
-	template<size_t I, size_t ... Is>
-	static constexpr size_t size_(std::index_sequence<Is...> const&)
-	{
-		return ( (Sectors*((I==Is)?1:0)) + ... );
+		using dummy_ = uint32_t[];
+		return ( dummy_{accu+=(Sectors * (Func{}(I,Is) ? 1 : 0)) ... }, accu);
 	}
 };
 
--- test/layout.cc
+++ test/layout.cc
@@ -5,21 +5,24 @@ using namespace flash_storage;
 
 TEST_CASE("layout")
 {
-	using layout = Layout<0x1001, 0x100, 0x2000, 0x5000, 0x5000 , 0x80000, 0x10000>;
+	using layout =
+		Layout<
+			BaseAddress<0x1001>,
+			Sizes<0x100, 0x2000, 0x5000, 0x5000 , 0x80000, 0x10000>>;
 
 	REQUIRE(layout::sector_count() == 6);
 
-	REQUIRE(layout::sector_offset<0>() == 0x1001);
-	REQUIRE(layout::sector_offset<1>() == 0x1101);
-	REQUIRE(layout::sector_offset<2>() == 0x3101);
-	REQUIRE(layout::sector_offset<3>() == 0x8101);
-	REQUIRE(layout::sector_offset<4>() == 0xd101);
-	REQUIRE(layout::sector_offset<5>() == 0x8d101);
+	static_assert(layout::sector_offset<0>() == 0x1001, "");
+	static_assert(layout::sector_offset<1>() == 0x1101, "");
+	static_assert(layout::sector_offset<2>() == 0x3101, "");
+	static_assert(layout::sector_offset<3>() == 0x8101, "");
+	static_assert(layout::sector_offset<4>() == 0xd101, "");
+	static_assert(layout::sector_offset<5>() == 0x8d101, "");
 
-	REQUIRE(layout::sector_size<0>() == 0x100);
-	REQUIRE(layout::sector_size<1>() == 0x2000);
-	REQUIRE(layout::sector_size<2>() == 0x5000);
-	REQUIRE(layout::sector_size<3>() == 0x5000);
-	REQUIRE(layout::sector_size<4>() == 0x80000);
-	REQUIRE(layout::sector_size<5>() == 0x10000);
+	static_assert(layout::sector_size<0>() == 0x100, "");
+	static_assert(layout::sector_size<1>() == 0x2000, "");
+	static_assert(layout::sector_size<2>() == 0x5000, "");
+	static_assert(layout::sector_size<3>() == 0x5000, "");
+	static_assert(layout::sector_size<4>() == 0x80000, "");
+	static_assert(layout::sector_size<5>() == 0x10000, "");
 }
